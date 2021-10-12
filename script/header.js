d760ae9a32f4c95ab888197175f68dc9181ee807
Alexandre CHARLIER <alexandre.charlier@free.fr>
2021-10-12 18:59:10 +0200

    menu hamburger with animate in progress

--- script/header.js
+++ script/header.js
@@ -1,5 +1,5 @@
 
-export default class Menu{
+/* export default class Menu{
     constructor(){
         const menu =document.getElementById("nav-icon");
         const toogleMenu = document.getElementById("toogleMenu");
@@ -14,6 +14,23 @@ export default class Menu{
                     dataOpen="false";
                     toogleMenu.dataset.toogle="false";
                 }
+        });
+    }
+} */
+
+export default class Menu {
+    constructor() {
+        const menu = document.getElementById("nav-icon");
+        const toogleMenu = document.getElementById("toogleMenu");
+        menu.addEventListener("click", function () {
+            if (toogleMenu.getAttribute("data-toogle") == "open") {
+                toogleMenu.setAttribute("data-toogle", "close");
+                menu.setAttribute("data-open", "false");
+            }
+            else {
+                menu.setAttribute("data-open", "true");
+                toogleMenu.setAttribute("data-toogle", "open");
+            }
         });
     }
 }
